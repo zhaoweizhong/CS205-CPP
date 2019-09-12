aa5621a146970eabe6b4f6b4f46a07b56ac90a55
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2019-09-12 14:02:00 +0800

    lab1

---
+++ lab1/name.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "钟兆玮" << endl;
+    cout << "This is my address." << endl;
+    return 0;
+}
---
+++ lab1/smile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+void smile();
+
+int main() {
+    smile();smile();smile();cout << endl;
+    smile();smile();cout << endl;
+    smile();cout << endl;
+    return 0;
+}
+
+void smile() {
+    cout << "Smile!";
+}
